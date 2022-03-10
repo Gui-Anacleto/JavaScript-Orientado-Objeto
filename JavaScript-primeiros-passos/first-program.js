d608eb4beabe3ed6857d0233e940e991b7981e9c
Guilherme de Freitas Anacleto <48931728+Paquista0@users.noreply.github.com>
2022-03-10 11:58:24 -0300

    first program 10/03/202 11:58

---
+++ JavaScript-primeiros-passos/first-program.js
@@ -0,0 +1,10 @@
+function cliente(cliente_1_Nome,cliente_1_Agencia,cliente_1_CPF,cliente_1_Saldo){
+    const cliente_1_Nome = "Ricardo";
+    const cliente_1_CPF = 11122233309;
+    const cliente_1_Agencia = 1001;
+    const cliente_1_Saldo = 0; 
+}
+
+console.log(cliente);
+
+
